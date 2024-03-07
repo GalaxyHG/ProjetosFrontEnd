08529acac8a1e9d7cb9a71943468e378bae6d2af
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2024-03-06 22:26:04 -0300

    Versão 6.9

---
+++ app2.js
@@ -0,0 +1,722 @@
+var elementos = [{
+    numero: "1",
+    simbolo: "H",
+    nome: "hidrogênio",
+    massa: "1,008"
+},
+{
+    numero: "2",
+    simbolo: "He",
+    nome: "hélio",
+    massa: "4,0026"
+},
+{
+    numero: "3",
+    simbolo: "Li",
+    nome: "lítio",
+    massa: "6,94"
+},
+{
+    numero: "4",
+    simbolo: "Be",
+    nome: "berílio",
+    massa: "9,0122"
+},
+{
+    numero: "5",
+    simbolo: "B",
+    nome: "boro",
+    massa: "10,81"
+},
+{
+    numero: "6",
+    simbolo: "C",
+    nome: "carbono",
+    massa: "12,011"
+},
+{
+    numero: "7",
+    simbolo: "N",
+    nome: "nitrogênio",
+    massa: "14,007"
+},
+{
+    numero: "8",
+    simbolo: "O",
+    nome: "oxigênio",
+    massa: "15,999"
+},
+{
+    numero: "9",
+    simbolo: "F",
+    nome: "flúor",
+    massa: "18,998"
+},
+{
+    numero: "10",
+    simbolo: "Ne",
+    nome: "neônio",
+    massa: "20,180"
+},
+{
+    numero: "11",
+    simbolo: "numero",
+    nome: "sódio",
+    massa: "22,990"
+},
+{
+    numero: "12",
+    simbolo: "Mg",
+    nome: "magnésio",
+    massa: "24,305"
+},
+{
+    numero: "13",
+    simbolo: "Al",
+    nome: "alumínio",
+    massa: "26,982"
+},
+{
+    numero: "14",
+    simbolo: "Si",
+    nome: "silício",
+    massa: "28,085"
+},
+{
+    numero: "15",
+    simbolo: "P",
+    nome: "fósforo",
+    massa: "30,974"
+},
+{
+    numero: "16",
+    simbolo: "S",
+    nome: "enxofre",
+    massa: "32,06"
+},
+{
+    numero: "17",
+    simbolo: "Cl",
+    nome: "cloro",
+    massa: "35,45"
+},
+{
+    numero: "18",
+    simbolo: "Ar",
+    nome: "argônio",
+    massa: "39,948"
+},
+{
+    numero: "19",
+    simbolo: "K",
+    nome: "potássio",
+    massa: "39,098"
+},
+{
+    numero: "20",
+    simbolo: "Ca",
+    nome: "cálcio",
+    massa: "40,078"
+},
+{
+    numero: "21",
+    simbolo: "Sc",
+    nome: "escândio",
+    massa: "44,956"
+},
+{
+    numero: "22",
+    simbolo: "Ti",
+    nome: "titânio",
+    massa: "47,867"
+},
+{
+    numero: "23",
+    simbolo: "V",
+    nome: "vanádio",
+    massa: "50,942"
+},
+{
+    numero: "24",
+    simbolo: "Cr",
+    nome: "crômio",
+    massa: "51,996"
+},
+{
+    numero: "25",
+    simbolo: "Mn",
+    nome: "manganês",
+    massa: "54,938"
+},
+{
+    numero: "26",
+    simbolo: "Fe",
+    nome: "ferro",
+    massa: "55,845"
+},
+{
+    numero: "27",
+    simbolo: "Co",
+    nome: "cobalto",
+    massa: "58,933"
+},
+{
+    numero: "28",
+    simbolo: "Ni",
+    nome: "níquel",
+    massa: "58,693"
+},
+{
+    numero: "29",
+    simbolo: "Cu",
+    nome: "cobre",
+    massa: "63,546"
+},
+{
+    numero: "30",
+    simbolo: "Zn",
+    nome: "zinco",
+    massa: "65,38"
+},
+{
+    numero: "31",
+    simbolo: "Ga",
+    nome: "gálio",
+    massa: "69,723"
+},
+{
+    numero: "32",
+    simbolo: "Ge",
+    nome: "germânio",
+    massa: "72,630"
+},
+{
+    numero: "33",
+    simbolo: "As",
+    nome: "arsênio",
+    massa: "74,922"
+},
+{
+    numero: "34",
+    simbolo: "Se",
+    nome: "selênio",
+    massa: "78,971"
+},
+{
+    numero: "35",
+    simbolo: "Br",
+    nome: "bromo",
+    massa: "79,904"
+},
+{
+    numero: "36",
+    simbolo: "Kr",
+    nome: "criptônio",
+    massa: "83,798"
+},
+{
+    numero: "37",
+    simbolo: "Rb",
+    nome: "rubídio",
+    massa: "85,468"
+},
+{
+    numero: "38",
+    simbolo: "Sr",
+    nome: "estrôncio",
+    massa: "87,62"
+},
+{
+    numero: "39",
+    simbolo: "Y",
+    nome: "ítrio",
+    massa: "88,906"
+},
+{
+    numero: "40",
+    simbolo: "Zr",
+    nome: "zircônio",
+    massa: "91,224"
+},
+{
+    numero: "41",
+    simbolo: "Nb",
+    nome: "nióbio",
+    massa: "92,906"
+},
+{
+    numero: "42",
+    simbolo: "Mo",
+    nome: "molibdênio",
+    massa: "95,95"
+},
+{
+    numero: "43",
+    simbolo: "Tc",
+    nome: "tecnécio",
+    massa: "[98]"
+},
+{
+    numero: "44",
+    simbolo: "Ru",
+    nome: "rutênio",
+    massa: "101,07"
+},
+{
+    numero: "45",
+    simbolo: "Rh",
+    nome: "ródio",
+    massa: "102,91"
+},
+{
+    numero: "46",
+    simbolo: "Pd",
+    nome: "paládio",
+    massa: "106,42"
+},
+{
+    numero: "47",
+    simbolo: "Ag",
+    nome: "prata",
+    massa: "107,87"
+},
+{
+    numero: "48",
+    simbolo: "Cd",
+    nome: "cádmio",
+    massa: "112,41"
+},
+{
+    numero: "49",
+    simbolo: "In",
+    nome: "índio",
+    massa: "114,82"
+},
+{
+    numero: "50",
+    simbolo: "Sn",
+    nome: "estanho",
+    massa: "118,71"
+},
+{
+    numero: "51",
+    simbolo: "Sb",
+    nome: "antimônio",
+    massa: "121,76"
+},
+{
+    numero: "52",
+    simbolo: "Te",
+    nome: "telúrio",
+    massa: "127,60"
+},
+{
+    numero: "53",
+    simbolo: "I",
+    nome: "iodo",
+    massa: "126,90"
+},
+{
+    numero: "54",
+    simbolo: "Xe",
+    nome: "xenônio",
+    massa: "131,29"
+},
+{
+    numero: "55",
+    simbolo: "Cs",
+    nome: "césio",
+    massa: "132,91"
+},
+{
+    numero: "56",
+    simbolo: "Ba",
+    nome: "bário",
+    massa: "137,33"
+},
+{
+    numero: "72",
+    simbolo: "Hf",
+    nome: "háfnio",
+    massa: "178,49"
+},
+{
+    numero: "73",
+    simbolo: "Ta",
+    nome: "tântalo",
+    massa: "180,95"
+},
+{
+    numero: "74",
+    simbolo: "W",
+    nome: "tungstênio",
+    massa: "183,84"
+},
+{
+    numero: "75",
+    simbolo: "Re",
+    nome: "rênio",
+    massa: "186,21"
+},
+{
+    numero: "76",
+    simbolo: "Os",
+    nome: "ósmio",
+    massa: "190,23"
+},
+{
+    numero: "77",
+    simbolo: "Ir",
+    nome: "irídio",
+    massa: "192,22"
+},
+{
+    numero: "78",
+    simbolo: "Pt",
+    nome: "platinumero",
+    massa: "195,08"
+},
+{
+    numero: "79",
+    simbolo: "Au",
+    nome: "ouro",
+    massa: "196,97"
+},
+{
+    numero: "80",
+    simbolo: "Hg",
+    nome: "mercúrio",
+    massa: "200,59"
+},
+{
+    numero: "81",
+    simbolo: "Tl",
+    nome: "tálio",
+    massa: "204,38"
+},
+{
+    numero: "82",
+    simbolo: "Pb",
+    nome: "chumbo",
+    massa: "207,2"
+},
+{
+    numero: "83",
+    simbolo: "Bi",
+    nome: "bismuto",
+    massa: "208,98"
+},
+{
+    numero: "84",
+    simbolo: "Po",
+    nome: "polônio",
+    massa: "[209]"
+},
+{
+    numero: "85",
+    simbolo: "At",
+    nome: "astato",
+    massa: "[210]"
+},
+{
+    numero: "86",
+    simbolo: "Rn",
+    nome: "radônio",
+    massa: "[222]"
+},
+{
+    numero: "87",
+    simbolo: "Fr",
+    nome: "frâncio",
+    massa: "[223]"
+},
+{
+    numero: "88",
+    simbolo: "Ra",
+    nome: "rádio",
+    massa: "[226]"
+},
+{
+    numero: "104",
+    simbolo: "Rf",
+    nome: "rutherfórdio",
+    massa: "[267]"
+},
+{
+    numero: "105",
+    simbolo: "Db",
+    nome: "dúbnio",
+    massa: "[268]"
+},
+{
+    numero: "106",
+    simbolo: "Sg",
+    nome: "seabórgio",
+    massa: "[269]"
+},
+{
+    numero: "107",
+    simbolo: "Bh",
+    nome: "bóhrio",
+    massa: "[270]"
+},
+{
+    numero: "108",
+    simbolo: "Hs",
+    nome: "hássio",
+    massa: "[269]"
+},
+{
+    numero: "109",
+    simbolo: "Mt",
+    nome: "meitnério",
+    massa: "[278]"
+},
+{
+    numero: "110",
+    simbolo: "Ds",
+    nome: "darmstádtio",
+    massa: "[281]"
+},
+{
+    numero: "111",
+    simbolo: "Rg",
+    nome: "roentgênio",
+    massa: "[281]"
+},
+{
+    numero: "112",
+    simbolo: "Cn",
+    nome: "copernício",
+    massa: "[285]"
+},
+{
+    numero: "113",
+    simbolo: "Nh",
+    nome: "nihônio",
+    massa: "[286]"
+},
+{
+    numero: "114",
+    simbolo: "Fl",
+    nome: "fleróvio",
+    massa: "[289]"
+},
+{
+    numero: "115",
+    simbolo: "Mc",
+    nome: "moscóvio",
+    massa: "[288]"
+},
+{
+    numero: "116",
+    simbolo: "Lv",
+    nome: "livermório",
+    massa: "[293]"
+},
+{
+    numero: "117",
+    simbolo: "Ts",
+    nome: "tenessino",
+    massa: "[294]"
+},
+{
+    numero: "118",
+    simbolo: "Og",
+    nome: "oganessônio",
+    massa: "[294]"
+},
+{
+    numero: "57",
+    simbolo: "La",
+    nome: "lantânio",
+    massa: "138,91"
+},
+{
+    numero: "58",
+    simbolo: "Ce",
+    nome: "cério",
+    massa: "140,12"
+},
+{
+    numero: "59",
+    simbolo: "Pr",
+    nome: "praseodímio",
+    massa: "140,91"
+},
+{
+    numero: "60",
+    simbolo: "Nd",
+    nome: "neodímio",
+    massa: "144,24"
+},
+{
+    numero: "61",
+    simbolo: "Pm",
+    nome: "promécio",
+    massa: "[145]"
+},
+{
+    numero: "62",
+    simbolo: "Sm",
+    nome: "samário",
+    massa: "150,36"
+},
+{
+    numero: "63",
+    simbolo: "Eu",
+    nome: "európio",
+    massa: "151,96"
+},
+{
+    numero: "64",
+    simbolo: "Gd",
+    nome: "gadolínio",
+    massa: "157,25"
+},
+{
+    numero: "65",
+    simbolo: "Tb",
+    nome: "térbio",
+    massa: "158,93"
+},
+{
+    numero: "66",
+    simbolo: "Dy",
+    nome: "disprósio",
+    massa: "162,50"
+},
+{
+    numero: "67",
+    simbolo: "Ho",
+    nome: "hólmio",
+    massa: "164,93"
+},
+{
+    numero: "68",
+    simbolo: "Er",
+    nome: "érbio",
+    massa: "167,26"
+},
+{
+    numero: "69",
+    simbolo: "Tm",
+    nome: "túlio",
+    massa: "168,93"
+},
+{
+    numero: "70",
+    simbolo: "Yb",
+    nome: "itérbio",
+    massa: "173,05"
+},
+{
+    numero: "70",
+    simbolo: "Lu",
+    nome: "lutécio",
+    massa: "174,97"
+},
+{
+    numero: "89",
+    simbolo: "Ac",
+    nome: "actínio",
+    massa: "[227]"
+},
+{
+    numero: "90",
+    simbolo: "Th",
+    nome: "tório",
+    massa: "232,04"
+},
+{
+    numero: "91",
+    simbolo: "Pa",
+    nome: "protactínio",
+    massa: "231,04"
+},
+{
+    numero: "92",
+    simbolo: "U",
+    nome: "urânio",
+    massa: "238,03"
+},
+{
+    numero: "93",
+    simbolo: "Np",
+    nome: "netúnio",
+    massa: "[237]"
+},
+{
+    numero: "94",
+    simbolo: "Pu",
+    nome: "plutônio",
+    massa: "[244]"
+},
+{
+    numero: "95",
+    simbolo: "Am",
+    nome: "amerício",
+    massa: "[243]"
+},
+{
+    numero: "96",
+    simbolo: "Cm",
+    nome: "cúrio",
+    massa: "[247]"
+},
+{
+    numero: "97",
+    simbolo: "Bk",
+    nome: "berquélio",
+    massa: "[247]"
+},
+{
+    numero: "98",
+    simbolo: "Cf",
+    nome: "califórnio",
+    massa: "[251]"
+},
+{
+    numero: "99",
+    simbolo: "Es",
+    nome: "einstênio",
+    massa: "[252]"
+},
+{
+    numero: "100",
+    simbolo: "Fm",
+    nome: "férmio",
+    massa: "[257]"
+},
+{
+    numero: "101",
+    simbolo: "Md",
+    nome: "mendelévio",
+    massa: "[258]"
+},
+{
+    numero: "102",
+    simbolo: "No",
+    nome: "nobélio",
+    massa: "[259]"
+},
+{
+    numero: "103",
+    simbolo: "Lr",
+    nome: "laurêncio",
+    massa: "[262]"
+}]
+
+function itemClicked(simbol) {
+    simbol -=1
+    document.getElementById('numc').innerHTML = elementos[simbol].numero;
+    document.getElementById('simbolo').innerHTML = elementos[simbol].simbolo;
+    document.getElementById('nomec').innerHTML = elementos[simbol].nome;
+    document.getElementById('numa').innerHTML = elementos[simbol].massa;
+
+    document.getElementById('nome').innerHTML = "<b>Nome: </b>" + elementos[simbol].nome;
+    document.getElementById('simbol').innerHTML = "<b>Símbolo: </b>" + elementos[simbol].simbolo;
+    document.getElementById('numero').innerHTML = "<b>Número: </b>" + elementos[simbol].numero;
+    document.getElementById('massa').innerHTML = "<b>Massa: </b>" + elementos[simbol].massa;
+    document.getElementById('cardItem').style.backgroundColor = 'rgba(58, 185, 19, 0.685)';
+}
